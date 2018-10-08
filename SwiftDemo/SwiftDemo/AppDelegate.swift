bbe2b01029cef8663e78e55537805e91106ac8f7
tryFighting <2893381320@qq.com>
2018-10-08 18:02:11 +0800

    swift基本数据类型学习，闭包（事先准备好的代码，需要调用时使用）

--- SwiftDemo/SwiftDemo/AppDelegate.swift
+++ SwiftDemo/SwiftDemo/AppDelegate.swift
@@ -14,11 +14,97 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
+   
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         print("Hello,World!")
+        array()
+        set()
+        dictionary()
+        closures() //TODO:闭包操作
         return true
     }
-
+    /// 闭包操作
+    func backWard(_ s1:String,_ s2: String) -> Bool {
+        return s1 > s2
+    }
+    func some(closures: () -> Void)  {
+        
+    }
+    func closures() -> () {
+       //闭包表达式
+        //单表达式闭包隐式返回
+        let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+        let reversedNames = names.sorted { (s1, s2) -> Bool in
+            s1 > s2
+        }
+        print(reversedNames)
+        
+        
+//        let  reverseNames = names.sorted(by: backWard)
+//        print(reverseNames)
+    }
+    /// 集合类型操作 Array,Sets,Dictionarues
+    func array() -> () {
+        var array = ["a","b","c"]
+        let add = ["1","2"]
+        array.append("d")
+        array.reverse()
+        array.removeFirst()
+        array += add
+        print(array)
+        //数组的遍历
+        for item in array {
+           print(item)
+        }
+        for (index,value) in array.enumerated() {
+            print("Item\(String(index + 1)):\(value)")
+        }
+    }
+    func set() -> () {
+        var  letters = Set<Character>()
+        letters.insert("a")
+        print(letters)
+        letters = []
+        print(letters)
+        var favoriteGenres: Set<String> = ["Rock","Classical","Hi hop"]
+        favoriteGenres.insert("Jazz")
+        print(favoriteGenres)
+        favoriteGenres.removeFirst()
+        print(favoriteGenres)
+        //遍历集合
+        for genre in favoriteGenres.sorted() {
+            print("\(genre)")
+        }
+        //集合基本操作
+        let oddDigit :Set = [1,3,5,7,9]
+        let evenDigit :Set = [0,2,4,6,8]
+        let signle :Set = [2,3,5,7]
+       let b = oddDigit.union(evenDigit).sorted()
+        print(oddDigit)
+      let c =  oddDigit.intersection(evenDigit).sorted()
+       let a = oddDigit.subtracting(signle).sorted()
+        print("\(a),\(b),\(c)")
+        //集合成员关系操作
+        let houseAnimals :Set = ["🐶","🐱"]
+        let farm :Set = ["🐂","🐔","🐑","🐶","🐱"]
+        let city :Set = ["🐦", "🐭"]
+       let e = houseAnimals.isSubset(of: farm)
+        let f = farm.isSuperset(of: houseAnimals)
+        let g = farm.isDisjoint(with: city)
+        print("\(e),\(f),\(g)")
+    }
+    func dictionary() -> () {
+        var airports: [String: String] = ["YYZ": "Toronto Pearson", "DUB": "Dublin"]
+        if airports.isEmpty{
+            
+        }else{
+            
+        }
+        airports["LHR"] = "London"
+        for (code,Name) in airports {
+            print("\(code):\(Name)")
+        }
+    }
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
--- SwiftDemo/SwiftDemo/TableViewController.swift
+++ SwiftDemo/SwiftDemo/TableViewController.swift
@@ -27,15 +27,67 @@ class TableViewController: UIViewController,UITableViewDataSource,UITableViewDel
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //设置该属性让contentview的x起点从navigationbar下面开始即x:64
+        //设置该属性让contentview的x起点从navigationbar下面开始即x:64  rawValue初始化
         self.edgesForExtendedLayout  = .init(rawValue: 0)
         mytableview = UITableView(frame: self.view.bounds, style: .plain)
         mytableview.delegate = self
         mytableview.dataSource = self
         mytableview.register(UITableViewCell.self, forCellReuseIdentifier: "cellID")
         view .addSubview(mytableview)
+        
+        print(sum(x: 10, y: 20))
+        print(sum1(num1: 60, num2: 70))
+        print(sum2(70, 80))
+        
+        print(defaultSum())
+        print(defaultSum(x: 10, y: 10))
+        print(defaultSum(x: 20))
+        print(defaultSum(y: 10))
+        
+        ///闭包函数调用无参数无返回值
+        let a = {
+            print("hello world")
+        }
+        a()
+        /// 函数闭包有参数有返回值
+        let  b = {
+            (x :Int,y: Int) in
+            return print(x + y)
+        }
+        b(10,20)
+        
+        DispatchQueue.global().async {
+            print("耗时操作\(Thread.current)")
+            DispatchQueue.main.async {
+                print("刷新UI\(Thread.current)")
+            }
+        }
+        loadData { (result) in
+            print(result)
+        }
+        
     }
-
+    func loadData(completion: (_ result: [String])->()) -> () {
+        let json = ["ssss","xxxx"]
+        completion(json)
+        
+    }
+    /// 函数值默认值调用
+    ///通过给参数设置默认值，可以组合调用，不传参，传默认
+    func defaultSum(x :Int = 1,y: Int = 2) -> Int {
+        return x + y
+    }
+    /// 函数调用求和
+    ///形参使用 _ 可以代替一切
+    func sum2(_ x:Int,_ y: Int) -> Int {
+        return x + y
+    }
+    func sum1(num1 x: Int, num2 y: Int) -> Int {
+        return x + y
+    }
+    func sum(x: Int,y: Int) -> Int {
+        return x + y
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
