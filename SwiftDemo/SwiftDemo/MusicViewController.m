f0062ec2e1ce7b9bee845f1e63bfc6ba938af91a
tryFighting <2893381320@qq.com>
2018-12-04 18:12:03 +0800

    音效文件工具类，录音生成，播放文件

--- SwiftDemo/SwiftDemo/MusicViewController.m
+++ SwiftDemo/SwiftDemo/MusicViewController.m
@@ -12,7 +12,8 @@
 #import <AVFoundation/AVFoundation.h>
 #import "MusicTool.h"
 @interface MusicViewController ()
-
+@property(nonatomic,strong)AVAudioPlayer *player;
+@property(nonatomic,strong)AVAudioRecorder *recorder;
 @end
 
 @implementation MusicViewController
@@ -31,6 +32,12 @@
     
     ///释放音效内存
     //AudioServicesDisposeSystemSoundID(soundID);
+}
+
+- (void)playMusic{
+    NSURL *url = [[NSBundle mainBundle] URLForResource:@"bg.mp3" withExtension:nil];
+    NSError *error;
+    self.player = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:&error];
 }
 
 
@@ -66,14 +73,56 @@
 
 
 
-
-
-
+- (void)record1{
+    NSArray *pathArr = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
+    NSString *path = [[pathArr lastObject] stringByAppendingPathComponent:@"record.wav"];
+    NSURL *fileUrl = [NSURL fileURLWithPath:path];
+    NSMutableDictionary *setting = [NSMutableDictionary dictionaryWithCapacity:1];
+    NSError *error;
+    NSLog(@"home == %@",NSHomeDirectory());
+    self.recorder = [[AVAudioRecorder alloc] initWithURL:fileUrl settings:setting error:&error];
+    
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    [MusicTool playShortMusic:@"tapped.caf" isSlient:NO];
+    //[MusicTool playShortMusic:@"tapped.caf" isSlient:NO];
+    [self playMusic];///播放器初始化操作
+    [self record1];///录音器初始化操作
+    NSArray *array = @[ @"播放/暂停", @"停止播放", @"录音", @"停止录音", @"我的订单", @"拇指理赔", @"身份证识别", @"手写签名回调", @"加载H5", @"我要续缴", @"电子保单下载" ];
+    for (int i = 0; i < array.count; i++) {
+        UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+        btn.frame = CGRectMake(50, 40 * (i) + 88, 200, 30);
+        btn.backgroundColor = UIColor.redColor;
+        [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        btn.tag = i;
+        [btn setTitle:array[i] forState:UIControlStateNormal];
+        [btn addTarget:self action:@selector(btnAct:) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:btn];
+    }
     
+}
+- (void)btnAct:(UIButton *)sender {
+    if (sender.tag == 0) {
+        self.player.isPlaying ? [self.player pause]:[self.player play];
+    }else if (sender.tag == 1){
+        [self.player stop];
+        self.player.currentTime = 0;
+    }else if (sender.tag == 2){
+        self.recorder.isRecording ? [self.recorder pause]:[self.recorder record];
+    }else if (sender.tag == 3){
+        [self.recorder stop];
+    }
+    
+//    if (sender.tag == 0) {
+//        //利用反射机制加载控制器
+//        Class my = NSClassFromString(@"NewReportViewController");
+//
+//        [self.navigationController pushViewController:[my new] animated:YES];
+//        //        MyInsuranceController *my = [MyInsuranceController new];
+//        //        my.hidesBottomBarWhenPushed = YES;
+//        //        [self.navigationController pushViewController:my animated:YES];
+//    }
 }
 ///接收内存警告 当前controller接收警告 全局则再appdelegate书写
 - (void)didReceiveMemoryWarning {
